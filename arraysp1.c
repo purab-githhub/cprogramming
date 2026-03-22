8d5269ccada84f7499709e17a1742ed590eb6758
purab-githhub <agarwal1purab1@gmail.com>
2026-03-22 11:30:36 +0530

    first array programm

--- arraysp1.c
+++ arraysp1.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-void main(){
-    int arrays[10]={0,1,4,9,16,};
+
+int main(){
+    int arrays[10]={0,1,4,9,16};
     int i ;
+
     for (i=5;i<10;i++)
     {
         arrays[i]=i*i;
-
     }
-    for (i=0;i<10;i++);
+
+    for (i=0;i<10;i++)
     {
         printf("arrays[%i]=%i\n",i,arrays[i]);
     }
+
+    return 0;
 }
