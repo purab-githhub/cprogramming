33b665480e6c863ae6678d2a69781f711b200b40
purab-githhub <agarwal1purab1@gmail.com>
2026-04-09 21:40:46 +0530

    assignment 16

---
+++ assignment16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main (){
+    int num ,i;
+    printf("enter the array size number:");
+    scanf("%d",&num);
+    int a[num];
+    printf("enter %d numbers:",num);
+    for (i=0;i<num;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Even numbers:");
+    for(i=0;i<num;i++){
+        if(a[i]%2==0){
+            printf("%d",a[i]);
+        }
+        
+    }
+    printf("Odd numbers:");
+    for(i=0;i<num;i++){
+        if(a[i]%2!=0){
+            printf("%d",a[i]);
+        }
+
+    }
+}
