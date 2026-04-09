492ec5a792454e73de647a51a0e375965542ce5a
purab-githhub <agarwal1purab1@gmail.com>
2026-04-09 20:24:07 +0530

    assignment 15

---
+++ assignment15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void main (){
+    int i,n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(i=0;i<n,i++){
+        printf("%d",rand());
+
+    }
+    
+}
