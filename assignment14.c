1b835ce2bbd7088906a0e20f782ea013bfccc883
purab-githhub <agarwal1purab1@gmail.com>
2026-04-09 20:06:10 +0530

    assignment 14

--- assignment14.c
+++ assignment14.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 void main (){
-    int bin , dec=0 , rem , i-0;
+    int bin , dec=0 , rem , i=0;
     printf("enter the binary number:");
     scanf("%d",&bin);
     while (bin>0){
         rem=bin%10;
-        dec+ = rem*pow(2,i);
+        dec += rem*pow(2,i);
         bin = bin/10;
         i++;
 
     }
-    printf("d",dec);
+    printf("%d",dec);
 
 
 } 
