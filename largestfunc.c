8a5fe60c138dab94e1c4c8c104ac7cfb729518d9
purab-githhub <agarwal1purab1@gmail.com>
2026-06-24 09:22:52 +0530

    largest of two through function

---
+++ largestfunc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void largest();
+void main()
+{
+    largest();
+}
+void largest()
+{
+    int a, b;
+    printf("enter two numers:");
+    scanf("%d  %d",&a,&b);
+    if (a>b){
+        printf("a is greater than b");
+    }
+        else{
+        printf("b is grater than a");
+
+        
+
+        }
+    }
