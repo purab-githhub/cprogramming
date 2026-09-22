2a65237648252efaac5fa0f4d46eb44a3f13a960
purab-githhub <agarwal1purab1@gmail.com>
2026-09-22 22:45:41 +0530

    coments for circular queue

--- cdsa/printercirculerqueue.c
+++ cdsa/printercirculerqueue.c
@@ -3,16 +3,22 @@
 #define n 5
 
 int Q[n];
+//intially the queue has nothing
 int front = 0, rear = 0;
 
 void AddCQ(int elem)
 {
+    /*now in linear queue we cant go back to the begining so this is not 
+    a problem in circular queue if suppose 0 1 2 3 4 then 4 can go back to 0 again
+    this is why rear+1 is there*/
+
+    //%n helps us to go to it like 1%5=1 type
     if ((rear + 1) % n == front)
     {
         printf("Queue Full\n");
     }
     else
-    {
+    {//eg Q[1]=101 addded
         rear = (rear + 1) % n;
         Q[rear] = elem;
         printf("Print Job %d added to queue.\n", elem);
@@ -20,9 +26,13 @@ void AddCQ(int elem)
 }
 
 void DelCQ()
+//we want to process the print job
 {
     int elem;
-
+//noraml checking condidtion alweays if queue is empty check front==rear
+//it check like suppose we added rear to two places than front will work 
+//from zero to that two places 
+//just to print the print job
     if (front == rear)
     {
         printf("Queue Empty\n");
@@ -45,13 +55,17 @@ void DisplayCQ()
     }
     else
     {
+        //front is 0
+        //rear has 3 values
         printf("Pending Print Jobs:\n");
+        //why front +1 beacuse fronts points to the position before the first position 
 
+        //i is 1
         i = (front + 1) % n;
 
         while (i != (rear + 1) % n)
-        {
-            printf("%d ", Q[i]);
+        {//1!=3 yes 
+            printf("%d ", Q[i]);//print tthe print jobs
             i = (i + 1) % n;
         }
 
